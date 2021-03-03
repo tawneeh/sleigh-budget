165f24388e4476a33cbc83d4042b68688cd93769
Tawnee Harris <tawneeh@icloud.com>
2021-03-02 17:26:35 -0800

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import Header from './Header';
+import './App.css';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+    </>
   );
 }
 
