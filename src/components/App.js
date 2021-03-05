37034db53a34b1311f93e65454a2768e880638f2
Tawnee Harris <tawneeh@icloud.com>
2021-03-05 15:10:16 -0800

    add routing to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Header from './Header';
 import GiftControl from './GiftControl';
+import Signin from './Signin';
 import './../App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <GiftControl />
-    </>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <GiftControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
