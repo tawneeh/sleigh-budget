261ea7e1071df12b01d7f2d11670711bd8596d4c
Tawnee Harris <tawneeh@icloud.com>
2021-03-05 11:11:29 -0800

    add firestoreReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import giftListReducer from './gift-list-reducer';
 import formVisibleReducer from './form-visible-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   masterGiftList: giftListReducer,
-  formVisibleOnPage: formVisibleReducer
+  formVisibleOnPage: formVisibleReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
