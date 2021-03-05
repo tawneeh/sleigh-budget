1f626213325bc02d6af1590dce62271358b94a70
Tawnee Harris <tawneeh@icloud.com>
2021-03-05 11:49:00 -0800

    add gifts to Firestore collection

--- src/components/NewGiftForm.js
+++ src/components/NewGiftForm.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
 import ReusableForm from './ReusableForm';
+import { useFirestore } from 'react-redux-firebase';
 
 function NewGiftForm(props) {
+
+  const firestore = useFirestore();
+  function addGiftToFirestore(event) {
+    event.preventDefault();
+
+    props.onNewGiftCreation();
+
+    return firestore.collection('gifts').add(
+      {
+        recipient: event.target.recipient.value,
+        giftName: event.target.giftName.value,
+        dollarAmount: parseInt(event.target.dollarAmount.value)
+      }
+    );
+  }
+
   return (
     <>
       <ReusableForm
-        formSubmissionHandler={handleNewGiftFormSubmission}
+        formSubmissionHandler={addGiftToFirestore}
         buttonText="Log a new Gift!" />
     </>
   );
 
-    function handleNewGiftFormSubmission(event) {
-    event.preventDefault();
-    props.onNewGiftCreation({recipient: event.target.recipient.value, giftName: event.target.giftName.value, dollarAmount: parseInt(event.target.dollarAmount.value), id: v4()});
-  }
+  //   function handleNewGiftFormSubmission(event) {
+  //   event.preventDefault();
+  //   props.onNewGiftCreation({recipient: event.target.recipient.value, giftName: event.target.giftName.value, dollarAmount: parseInt(event.target.dollarAmount.value), id: v4()});
+  // }
+
 }
 
 NewGiftForm.propTypes = {
