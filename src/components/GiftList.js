966e58a78f9aa8fded201ce087d75561496aa63b
Tawnee Harris <tawneeh@icloud.com>
2021-03-05 10:01:53 -0800

    add list component

---
+++ src/components/GiftList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Gift from "./Gift";
+import PropTypes from "prop-types";
+
+function GiftList(props) {
+  return (
+    <>
+      <hr />
+      {Object.values(props.giftList).map((gift) => {
+        return <Gift
+          whenGiftClicked = { props.onGiftSelection }
+          recipient={gift.recipient}
+          giftName={gift.giftName}
+          dollarAmount={gift.dollarAmount}
+          id={gift.id}
+          key={gift.id} />
+      })}
+    </>
+  );
+}
+
+GiftList.propTypes = {
+  giftList: PropTypes.object,
+  onGiftSelection: PropTypes.func
+};
+
+export default GiftList;
