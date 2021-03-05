269bc02d175a9c93170f99d9d333de9af5493d7b
Tawnee Harris <tawneeh@icloud.com>
2021-03-05 15:36:14 -0800

    add a link to Signin component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
       <h1>Welcome to SleighBudget!</h1>
       <h3><em>Slay the Season</em></h3>
+        <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
     </>
   );
 }
