133a6b400edbc110440aa45f0a9ad82da287cbf7
Tawnee Harris <tawneeh@icloud.com>
2021-03-05 17:04:58 -0800

    add Dark Mode components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import GiftControl from './GiftControl';
 import Signin from './Signin';
-import './../App.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './GlobalStyles';
+import { lightTheme, darkTheme } from './Themes.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './../App.css';
 
 function App() {
+
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/signin">
-          <Signin />
-        </Route>
-        <Route path="/">
-          <GiftControl />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+        <Router>
+          <Header />
+          <button onClick={themeToggler}>Nightmare Before Christmas Mode</button>
+          <Switch>
+            <Route path="/signin">
+              <Signin />
+            </Route>
+            <Route path="/">
+              <GiftControl />
+            </Route>
+          </Switch>
+        </Router>
+    </ThemeProvider>
   );
 }
 
