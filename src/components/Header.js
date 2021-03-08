b4e45e4ea4579bd1f471ff41595ba2d771afba7e
Tawnee Harris <tawneeh@icloud.com>
2021-03-08 10:34:41 -0800

    adjust styling in favor of light mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
-  background-color: grey;
+  background-color: red;
   text-align: center;
-  color: black;
+  color: white;
+  padding-bottom: 15px;
 `;
 
 function Header() {
@@ -14,6 +15,7 @@ function Header() {
       <StyledHeader>
         <h1>Welcome to SleighBudget!</h1>
         <h2><em>Slay the Season</em></h2>
+      </StyledHeader>
           <ul>
           <li>
             <Link to="/">Home</Link>
@@ -22,7 +24,6 @@ function Header() {
             <Link to="/signin">Sign In</Link>
           </li>
         </ul>
-      </StyledHeader>
     </>
   );
 }
