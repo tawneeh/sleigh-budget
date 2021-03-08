103914163aa79d3ae213b7284855b2bd3e5493d7
Tawnee Harris <tawneeh@icloud.com>
2021-03-08 07:48:47 -0800

    add a comment

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { func, string } from 'prop-types';
 import styled from 'styled-components';
 
-const Button = styled.button`
+const Button = styled.button` {/* can make this button an icon instead of a reg button using styled-components!!! jack-o-lantern! */}
   background: ${({ theme }) => theme.background};
   border: 2px solid ${({ theme }) => theme.toggleBorder};
   color: ${({ theme }) => theme.text};
