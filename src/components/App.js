3562cbf7dd6d8cba4e23dce0d3b5f8aec39d1d72
Tawnee Harris <tawneeh@icloud.com>
2021-03-07 19:39:59 -0800

    update useDarkMode with mountedComponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import GiftControl from './GiftControl';
 import Signin from './Signin';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './GlobalStyles';
 import { lightTheme, darkTheme } from './Themes.js';
+import Toggle from './Toggler';
+import { useDarkMode } from './useDarkMode';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './../App.css';
 
 function App() {
 
-  const [theme, setTheme] = useState('light');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
-  }
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
+  if (!mountedComponent) return <div />
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    <ThemeProvider theme={themeMode}>
       <GlobalStyles />
         <Router>
           <Header />
-          <button onClick={themeToggler}>Nightmare Before Christmas Mode</button>
+            <Toggle theme={theme} toggleTheme={themeToggler} /> {/* this determines the toggle button location -- inline styling for placement?*/}
           <Switch>
             <Route path="/signin">
               <Signin />
