2b3c0c03a807dbd37c8df9788bf4c63675ded8bc
Tawnee Harris <tawneeh@icloud.com>
2021-03-08 10:31:02 -0800

    update styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background-color: grey;
+  text-align: center;
+  color: black;
+`;
 
 function Header() {
   return (
     <>
-      <h1>Welcome to SleighBudget!</h1>
-      <h3><em>Slay the Season</em></h3>
-        <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/signin">Sign In</Link>
-        </li>
-      </ul>
+      <StyledHeader>
+        <h1>Welcome to SleighBudget!</h1>
+        <h2><em>Slay the Season</em></h2>
+          <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signin">Sign In</Link>
+          </li>
+        </ul>
+      </StyledHeader>
     </>
   );
 }
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,6 +1,6 @@
 export const lightTheme = {
     body: '#FFF',
-    text: '#363537',
+    text: '#EB0300',
     toggleBorder: '#FFF',
     background: '#363537',
 }
