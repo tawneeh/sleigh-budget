9d4166993d348a90965fa6e49c01de1ed3fbe1b1
Tawnee Harris <tawneeh@icloud.com>
2021-03-09 09:40:54 -0800

    add Styled Component

---
+++ src/components/StyledButton.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border: none;
+  border-radius: 7px;
+  margin-left: 7px;
+  margin-top: 7px;
+  margin-bottom: 7px;
+  cursor: pointer;
+  color: white;
+  background-color: black;
+  font-size: 10px;
+  font-family: arial;
+  padding: 7px 7px 7px 7px;
+`
+
+export default StyledButton;
