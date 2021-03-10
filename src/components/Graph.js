4c25cb193e5387f7120deb040a4256203d81c963
Tawnee Harris <tawneeh@icloud.com>
2021-03-09 16:23:23 -0800

    attempt to fetch data from Firestore for Graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,31 +1,35 @@
-import React, { useEffect } from 'react';
-import Chart from 'chart.js';
+import React, { useState, useEffect } from 'react';
+//import Chart from 'chart.js';
+import firebase from './../firebase';
+//import * as data from './gift.json'; (only if using JSON Object file)
 
 function Graph() {
+
+  const [gifts, setGifts] = useState([]);
+  const fetchGifts = async() => {
+    const response = firebase.collection('gifts');
+    const data = await response.get();
+    data.docs.forEach(item => {
+      setGifts([...gifts, item.data()])
+    })
+  }
+
   useEffect(() => {
-    const ctx = document.getElementById("giftTotal");
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Saved", "Needed"],
-        datasets: [
-          {
-            label: "Dollar Bills",
-            data: [10, 80, 0],
-            backgroundColor: [
-              "Red",
-              "Green"
-            ],
-            borderColor: ["Red", "Green"],
-            borderWidth: 1
-          }
-        ]
-      }
-    });
-  });
+    fetchGifts();
+  }, [])
+
   return (
     <>
-      <canvas id="giftTotal" width="100" height="100" />
+      {
+        gifts && gifts.map(gift => {
+          return (
+          <>
+            <h1>{gift.dollarAmount}</h1>
+            {/* <canvas id="giftTotal" width="100" height="100" /> */}
+          </>
+          )
+        })
+      }
     </>
   );
 }
