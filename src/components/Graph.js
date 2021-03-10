178e1c3226587105c942af6a23abcb60090932eb
Tawnee Harris <tawneeh@icloud.com>
2021-03-10 11:38:05 -0800

    add bracket notation to access dollarAmount correctly

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
 import Chart from 'chart.js';
+import PropTypes from 'prop-types';
 
 function Graph(props) {
+  //const { gift } = props; // remove this and console.log(props) if issues happen
 
   console.log(props, "graph line 6"); // passing down gift object - object within an object
-  console.log(props.dollarAmount); // undefined
+  console.log(props[0].dollarAmount); // not undefined when adding the bracket notation!
 
   // const total = gifts.map((gift) => {
   //   // loop through gifts and add all of the dollarAmount properties together
   //   // gift.dollarAmount 
   // })
 
-  const total = Object.entries(props) {
-    
-  }
+  // const total = Object.entries(gift) {
+
+  // }
 
   useEffect(() => {
     const ctx = document.getElementById("giftTotal");
@@ -24,7 +26,7 @@ function Graph(props) {
         datasets: [
           {
             label: "Dollar Bills",
-            data: [10, 50, 0], // can hard code a dollarAmount in GiftList to pass down if needed
+            data: [10, props[0].dollarAmount, 0], // can hard code a dollarAmount in GiftList to pass down if needed
             backgroundColor: [
               "Red",
               "Green"
@@ -40,9 +42,13 @@ function Graph(props) {
   return (
     <>
       <canvas id="giftTotal" width="40" height="40" />
-      <h3>{props.dollarAmount}</h3>
+      <h3>{props[0].dollarAmount}</h3>
     </>
   );
 }
+
+Graph.propTypes = {
+  gift: PropTypes.object
+};
 
 export default Graph;
