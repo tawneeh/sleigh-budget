876b9cf4f25f9402e5d386d875eb99c257b6d3d2
Tawnee Harris <tawneeh@icloud.com>
2021-03-10 16:54:31 -0800

    add Links into the StyledHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,21 +11,26 @@ const StyledHeader = styled.header`
   padding-top: 25px;
 `;
 
+const StyledNav = styled.div`
+  text-decoration: none;
+  text-align: right;
+  color: white;
+  margin-right: 10px;
+`;
+
 function Header() {
   return (
     <>
       <StyledHeader>
         <h1>SleighBudget</h1>
         <h2><em>Slay the Season</em></h2>
+          <StyledNav>
+            <Link to="/" style={{color: "white", textDecoration: "none"}}>Home</Link>
+            <br />
+            <Link to="/signin" style={{color: "white", textDecoration: "none"}}>Account</Link>
+          </StyledNav>
       </StyledHeader>
-        <ul style={{listStyleType: "none"}}>
-          <li>
-            <Link to="/" style={{color: "red", textDecoration: "none"}}>Home</Link>
-          </li>
-          <li>
-            <Link to="/signin" style={{color: "red", textDecoration: "none"}}>Account</Link>
-          </li>
-        </ul>
+
     </>
   );
 }
